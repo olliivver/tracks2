1f302a1660253a9c5104649e9b58b3630aab036b
Oliver Mercer-Smail <oliver.finlay@gmail.com>
2023-01-12 15:44:16 -0500

    server adjustment

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const helmet = require("helmet");
 const morgan = require("morgan");
 require("dotenv").config();
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -19,14 +20,33 @@ const {
   policeReportTimes,
 } = require("./handlers");
 
+app.use(function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, HEAD, GET, PUT, POST, DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+    res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 app.use(express.json())
 app.use(helmet())
 app.use(morgan('tiny'))
+app.use(express.urlencoded({ extended: false }));
+app.use("/", express.static(__dirname + "/"));
+app.use(
+  cors({
+    origin: ["https://tracks-v2.onrender.com/", "https://www.mtltracks.ca/"],
+  })
+);
 app.use(express.static("../build"))
 
 
 app.get("/", (req, res) => {
-  res.status(200).json({status: 200, message: "Penis World!"});
+  res.status(200).json({status: 200, message: "Hello World!"});
 });
 
 app.get("/get-crossing", getCrossing)
